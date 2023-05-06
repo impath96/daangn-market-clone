fdb925a68a5523a33935cff72e99c0a648dc4b08
impath96 <impath96@gmail.com>
2023-05-06 15:19:38 +0900

    chore : init project

---
+++ src/main/java/com/zerobase/daangnmarketclone/DaangnMarketCloneApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.daangnmarketclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaangnMarketCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaangnMarketCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/daangnmarketclone/DaangnMarketCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.daangnmarketclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaangnMarketCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
