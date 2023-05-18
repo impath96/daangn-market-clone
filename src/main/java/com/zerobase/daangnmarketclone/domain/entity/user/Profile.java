690f9b8a49bd180b9ce5818aa012051691599811
impath96 <impath96@gmail.com>
2023-05-18 22:05:15 +0900

    refactor : nickname과 imageUrl 을 Profile 로 변경

---
+++ src/main/java/com/zerobase/daangnmarketclone/domain/entity/user/Profile.java
@@ -0,0 +1,18 @@
+package com.zerobase.daangnmarketclone.domain.entity.user;
+
+import javax.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class Profile {
+
+    private String nickname;
+
+    private String imageUrl;
+
+}
