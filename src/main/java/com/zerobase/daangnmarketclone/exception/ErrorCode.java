eb113baa69b551c451b0049f1b099a08545719f6
impath96 <impath96@gmail.com>
2023-05-25 17:16:26 +0900

    refactor : ErrorCode enum 마침표 위치 변경

--- src/main/java/com/zerobase/daangnmarketclone/exception/ErrorCode.java
+++ src/main/java/com/zerobase/daangnmarketclone/exception/ErrorCode.java
@@ -8,7 +8,8 @@ public enum ErrorCode {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
-    IMAGE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, "이미지 업로드에 실패했습니다");
+    IMAGE_UPLOAD_FAIL(HttpStatus.BAD_REQUEST, "이미지 업로드에 실패했습니다"),
+    ;
 
     private final HttpStatus status;
     private final String message;
